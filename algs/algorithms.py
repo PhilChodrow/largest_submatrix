dfc2c8248feff3faf2c6ecef05853b4ca9bc32f7
Philip Samuel Chodrow <pchodrow@gmail.com>
2018-05-07 19:06:32 -0400

    cleaning and comments

--- algs/algorithms.py
+++ algs/algorithms.py
@@ -1,47 +1,54 @@
 import numpy as np
 
-def greedy_search(k, method = 'LAS'):
-	'''
-		init should also include intersection of row/column with largest sums
-		method in {'LAS', 'IGP'}
-		Methods from https://arxiv.org/pdf/1602.08529.pdf
-	'''
-	if method == 'LAS':
-		return LAS_search(k)
-	else: 
-		return IGP_search(k)
-
-	print 'not implemented'
-
 def submatrix(A, ix):
+	'''
+	Get the submatrix corresponding to a given set of row and column indices. 
+	A: a 2d np.array
+	ix: dict of the form {'rows'    : np.array([1, 2, 3, 4]),
+						  'columns' : np.array([1, 2, 3, 4])}
+	'''
     return A[ix['rows'][:, np.newaxis], ix['columns']]
 
 def value(A, ix):
+	'''
+	Get the value (average) of a submatrix corresponding to a given set of row 
+	and column indices. 
+	A: a 2d np.array
+	ix: dict of the form {'rows'    : np.array([1, 2, 3, 4]),
+						  'columns' : np.array([1, 2, 3, 4])}
+	'''
     return np.mean(submatrix(A, ix))
 
-def LAS(A, k, n_iters = 1):
+def LAS(A, k):
     '''
     	Algorithm LAS from https://arxiv.org/pdf/1602.08529.pdf
+    	A: a 2d np.array
+    	k: a positive integer
     '''
     m, n = A.shape
 
+    # given a fixed set of row indices, find the k columns whose sums 
+    # in those indices are largest. 
     def column_update():
-        sums = A[ix['rows'],:].sum(axis = 0)
-        new_ix = np.argpartition(-sums, k)[0:k]
-        ix['columns'] = new_ix
+        sums = A[ix['rows'],:].sum(axis = 0)     # restrict to given row indices
+        new_ix = np.argpartition(-sums, k)[0:k]  # find k largest restricted cols
+        ix['columns'] = new_ix                   # replace old indices with new
 
+    # given a fixed set of column indices, find the k rows whose sums 
+    # in those indices are largest. 
     def row_update():
-        sums = A[:, ix['columns']].sum(axis = 1)
-        new_ix = np.argpartition(-sums, k)[0:k]
-        ix['rows'] = new_ix
+        sums = A[:, ix['columns']].sum(axis = 1) # restrict to given column indices
+        new_ix = np.argpartition(-sums, k)[0:k]  # find the k largest restricted rows
+        ix['rows'] = new_ix                      # replace old indices with new
 
-    # initialize
+    # randomly initialize indices
     ix = {'rows'    : np.random.choice(m, k, replace=False),
           'columns' : np.random.choice(n, k, replace=False)}    
     
+    # initialize loop parameters. 
     prev_val = -10**(8)
     val = 0
-    eps = 10**(-8)
+    eps = 10**(-8) # tolerance: when improvement in val is less than this, stop.
   
     # main loop
     while np.abs(val - prev_val) > eps:
@@ -50,42 +57,49 @@ def LAS(A, k, n_iters = 1):
         column_update()
         val = value(A, ix)
     
-    # return result as dict
+    # return result as dict, including the indices and the value. 
     return({'ix' : ix, 'val': val})
 	
 def IGP(A, k):
 	'''
     	Algorithm IGP from https://arxiv.org/pdf/1602.08529.pdf
+    	A: a 2d np.array
+    	k: a positive integer
     '''
 
     m, n = A.shape
 
+    # partition the row and column indices into k+1 approximately-equal segments 
+    # R_i and C_i for i in 1...k+1
     row_part = range(0, m+1, m/k)
     col_part = range(0, n+1, n/k)
 
+    # fixing current row indices, search through the columns within C_i the the one
+    # that most increases the value. 
     def column_update(i):
-        C_n = np.arange(col_part[i-1], col_part[i])
-        A_sub = submatrix(A, 
-                 {'rows'   : ix['rows'],
-                  'columns': C_n})
+        C_i = np.arange(col_part[i-1], col_part[i]) # grab C_i
+        A_sub = submatrix(A, 						# restrict to columns in C_i
+                 {'rows'   : ix['rows'],            # and rows in ix
+                  'columns': C_i})
 
-        new = np.argmax(A_sub.sum(axis = 0))
+        new = np.argmax(A_sub.sum(axis = 0))        # index of best addition in C_i
 
-        ix['columns'] = np.append(ix['columns'], C_n[new]).astype('int')
+        # add best addition to ix
+        ix['columns'] = np.append(ix['columns'], 
+                                  C_i[new]).astype('int')
 
     def row_update(i):
-        R_n = np.arange(row_part[i-1], row_part[i])
+        R_i = np.arange(row_part[i-1], row_part[i])   # grab R_i
 
-        A_sub = submatrix(A,
-                          {'rows'   : R_n,
+        A_sub = submatrix(A,                          # restrict to rows in R_i
+                          {'rows'   : R_i,            # and columns in ix
                            'columns': ix['columns']})
-        new = np.argmax(A_sub.sum(axis = 1))
+        new = np.argmax(A_sub.sum(axis = 1))          # index of best addition in R_i
 
-        ix['rows'] = np.append(ix['rows'], R_n[new]).astype('int')
+        # add best addition to ix
+        ix['rows'] = np.append(ix['rows'], 
+                               R_i[new]).astype('int')
 
-        i += 1
-    
-    
     # initialize
     ix = {'rows'    : np.random.choice(row_part[1], 1),
           'columns' : []}     
@@ -99,5 +113,6 @@ def IGP(A, k):
         column_update(i)
         i += 1
     
+    # output as dict including indices and value
     return {'ix': ix, 
             'val': value(A, ix)}
