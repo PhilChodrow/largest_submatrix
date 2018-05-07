1af6af8ddcc225b902e4ea314a165a685b8b382e
Philip Samuel Chodrow <pchodrow@gmail.com>
2018-05-07 10:12:45 -0400

    read_data

---
+++ algs/utils.py
@@ -0,0 +1,17 @@
+
+import numpy as np
+
+def read_data(path):
+	m = np.loadtxt(path)
+	m[:,0:2] = m[:,0:2] / (2.5*10**5)
+
+	ix = m[:,0:2].astype('int')
+
+	container = np.zeros((max(ix[:,0])+1, 
+	                      max(ix[:,1])+1))
+
+	for i in range(ix.shape[0]):
+	    ix_i = ix[i,:]
+	    container[ix_i[0], ix_i[1]] = m[i,2]
+
+	return container
